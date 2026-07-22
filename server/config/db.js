5bc5c593cd7ac09876dc36d391a0e91cf9b93d04
MayankDalal19 <iammayank19042007@gmail.com>
2026-07-22 10:17:45 +0530

    second

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // Connects to MongoDB using the URI from environment variables
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect('mongodb://Capstone:cap123@ac-rgamfmq-shard-00-00.eltxqn6.mongodb.net:27017,ac-rgamfmq-shard-00-01.eltxqn6.mongodb.net:27017,ac-rgamfmq-shard-00-02.eltxqn6.mongodb.net:27017/?ssl=true&replicaSet=atlas-jcec1s-shard-0&authSource=admin&appName=Cluster0');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
